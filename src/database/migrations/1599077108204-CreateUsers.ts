0ffe68f022901754c2785c9d22b2b439e4a5a607
hiagopsilva <hiagohps360@gmail.com>
2020-09-03 16:17:20 -0300

    update migrations

--- src/database/migrations/1599077108204-CreateUsers.ts
+++ src/database/migrations/1599077108204-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1599077108204 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
