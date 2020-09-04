f2cd649d69a045e688ba1e3b2a78b0aa1d684c84
hiagopsilva <hiagohps360@gmail.com>
2020-09-04 14:54:03 -0300

    add class AppError

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,6 +1,7 @@
 import { getRepository } from 'typeorm';
 import { hash } from 'bcryptjs';
 
+import AppError from '../errors/AppError';
 import User from '../models/user';
 
 interface Request {
@@ -18,7 +19,7 @@ class CreateUserService {
     });
 
     if (checkUserExists) {
-      throw Error('Email addres already used.');
+      throw new AppError('Email addres already used.');
     }
 
     const hashedPassword = await hash(password, 8);
