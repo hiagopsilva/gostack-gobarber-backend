8b3b63915c268d519375e91f05efbe16c98f374c
hiagopsilva <hiagohps360@gmail.com>
2020-09-04 01:40:44 -0300

    add authenticate

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,7 +1,9 @@
 import { getRepository } from 'typeorm';
-import { hash, compare } from 'bcryptjs';
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 
 import User from '../models/user';
+import authConfig from '../config/auth';
 
 interface Request {
   email: string;
@@ -10,6 +12,7 @@ interface Request {
 
 interface Response {
   user: User;
+  token: string;
 }
 
 class AuthenticateUserSerice {
@@ -28,8 +31,16 @@ class AuthenticateUserSerice {
       throw new Error('Incorrect email/password combination.');
     }
 
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = sign({}, secret, {
+      subject: user.id,
+      expiresIn,
+    });
+
     return {
       user,
+      token,
     };
   }
 }
